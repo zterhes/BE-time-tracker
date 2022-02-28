8817036c443f97bf3b68f04d6639d4faf535a6a4
zterhes <zterhes1@gmail.com>
2022-02-28 16:35:30 +0100

    refactor file tree

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
